f40a3f9beac6650ea60e1729ad82e9b358d8e8d3
Максим Овчинников <ovchinnikov.mv@reliab.tech>
2021-01-23 15:06:48 +0300

    Initial commit

---
+++ app/src/main/java/ru/tech_reliab/githubrepos/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.tech_reliab.githubrepos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
